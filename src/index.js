efc915a98f6dad21a06a932c2f1d23b165d77866
Michael Roberts <DoloMike@users.noreply.github.com>
2018-12-16 16:16:12 -0600

    add travis ci, make src directory, give roomApi a more accurate name (roomController)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// load .env values from this projects root directory
+require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
+
+// require express, socket.io, and our chat room controller
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const roomController = require('./roomController');
 const port = process.env.port || 3000;
-const roomApi = require('./roomApi');
 
 io.origins('*:*'); // allow cors
 
 io.on('connection', async (socket) => {
 	// get list of chat rooms to join
 	console.log('a user connected');
-	const rooms = await roomApi.getRooms();
+	const rooms = await roomController.getRooms();
 	socket.emit('gotRooms', rooms);
 
 	// client can only join one chat room at a time
@@ -23,7 +27,7 @@ io.on('connection', async (socket) => {
 	// get the full room document
 	socket.on('joinRoom', async (roomId) => {
 		safeJoin(roomId);
-		room = await roomApi.getRoom(roomId);
+		room = await roomController.getRoom(roomId);
 		socket.emit('room', room);
 	});
 
